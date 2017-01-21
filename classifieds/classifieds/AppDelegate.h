044c6818a6b2d2c4c428c3cc70611e41e25f7975
Stefan Kramreither <me@stefankram.com>
2017-01-21 00:18:18 -0500

    Base project

---
+++ classifieds/classifieds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  classifieds
+//
+//  Created by Stefan Kramreither on 2017-01-21.
+//  Copyright © 2017 Stefan Kramreither. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
