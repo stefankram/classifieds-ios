436d9fba791f8e13577b3fe871f4a501892cc290
Stefan Kramreither <me@stefankram.com>
2017-02-18 13:34:07 -0500

    Base project

--- classifieds/classifieds/main.m
+++ classifieds/classifieds/main.m
@@ -1,15 +1,16 @@
 //
-//  Created by Stefan Kramreither on 2017-01-21.
+//  main.m
+//  classifieds
+//
+//  Created by Stefan Kramreither on 2017-02-18.
 //  Copyright © 2017 Stefan Kramreither. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char *argv[])
-{
-    @autoreleasepool
-    {
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
